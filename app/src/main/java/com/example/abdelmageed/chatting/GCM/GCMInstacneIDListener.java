60d29d32890ed0a46354f26c77b2e71a26aa1a23
abdelmageedesmail <abdelmageedalghor44@gmail.com>
2016-06-22 03:07:09 +0200

    Add files via upload

---
+++ app/src/main/java/com/example/abdelmageed/chatting/GCM/GCMInstacneIDListener.java
@@ -0,0 +1,24 @@
+package com.example.abdelmageed.chatting.GCM;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import com.google.android.gms.iid.InstanceIDListenerService;
+
+/**
+ * Created by abdelmageed on 20/06/16.
+ */
+public class GCMInstacneIDListener extends InstanceIDListenerService {
+
+    SharedPreferences preferences;
+    SharedPreferences.Editor prefEditor;
+
+    @Override
+    public void onTokenRefresh() {
+        preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        prefEditor = preferences.edit();
+        prefEditor.putBoolean("token_sent", false).apply();
+        startService(new Intent(this, RegistrationServices.class));
+    }
+}
---
+++ app/src/main/java/com/example/abdelmageed/chatting/GCM/RegistrationServices.java
@@ -0,0 +1,85 @@
+package com.example.abdelmageed.chatting.GCM;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+import com.google.android.gms.iid.InstanceID;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by abdelmageed on 18/06/16.
+ */
+public class RegistrationServices extends IntentService{
+    SharedPreferences preferences;
+    SharedPreferences.Editor prefEditor;
+
+
+    public RegistrationServices() {
+        super("RegistrationService");
+    }
+
+
+
+    @Override
+    protected void onHandleIntent(Intent intent){
+
+        preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        prefEditor = preferences.edit();
+
+
+        InstanceID instanceID= InstanceID.getInstance(this);
+        try {
+            String token = instanceID.getToken("937347683419", GoogleCloudMessaging.INSTANCE_ID_SCOPE, null);
+            if (!preferences.getBoolean("token_sent", false))
+                sendTokenToServer(token);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void sendTokenToServer(final String token){
+        String ADD_TOKEN_URL = "http://emtyazna.com/mohamed/chating/activities/registerGcm";
+        StringRequest stringRequest=new StringRequest(Request.Method.POST, ADD_TOKEN_URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                int responseCode = Integer.parseInt(response);
+                if (responseCode == 1) {
+                    prefEditor.putBoolean("token_sent", true).apply();
+                    Log.e("Registration Service", "Response : Send Token Success");
+                } else {
+                    prefEditor.putBoolean("token_sent", false).apply();
+                    Log.e("Registration Service", "Response : Send Token Failed");
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                prefEditor.putBoolean("token_sent", false).apply();
+                Log.e("Registration Service", "Error :Send Token Failed");
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+
+                Map<String, String> params = new HashMap<>();
+                params.put("token", token);
+                return params;
+            }
+        };
+        Volley.newRequestQueue(this).add(stringRequest);
+    }
+}
